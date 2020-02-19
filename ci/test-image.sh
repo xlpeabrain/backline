3481ee6f2ad271a9137d61e7adf342346003db74
Jason Hoong Kit Lee <hklee@pivotal.io>
2020-02-19 22:58:31 +0800

    DIND test

--- ci/test-image.sh
+++ ci/test-image.sh
@@ -8,16 +8,17 @@ start_docker
 cd backline-src
 ls
 docker run -d --rm --name backline -p 8080:8080 xlpeabrain/backline
-curl localhost:8080
+sleep 30
+#curl http://localhost:8080/actuator/health
 
 
-#RESULT=$(curl http://localhost:8080/actuator/health)
-#echo $RESULT
-#if [[ $RESULT == *"UP"* ]];
-#then
-#  echo "Up"
-#  exit 0
-#else
-#  echo "Down"
-#  exit 1
-#fi
+RESULT=$(curl http://localhost:8080/actuator/health)
+echo $RESULT
+if [[ $RESULT == *"UP"* ]];
+then
+  echo "Up"
+  exit 0
+else
+  echo "Down"
+  exit 1
+fi
