3f9d0dddb247b9bea02891f2c84db2a7977d8a2b
Jason Hoong Kit Lee <hklee@pivotal.io>
2020-02-19 18:12:17 +0800

    DIND

--- ci/test-image.sh
+++ ci/test-image.sh
@@ -3,13 +3,21 @@
 sudo apt update && sudo apt upgrade
 sudo apt install curl
 
-RESULT=$(curl http://localhost:8080/actuator/health)
-echo $RESULT
-if [[ $RESULT == *"UP"* ]];
-then
-  echo "Up"
-  exit 0
-else
-  echo "Down"
-  exit 1
-fi
+source /docker-lib.sh
+start_docker
+
+cd backline-src
+ls
+docker run --rm --name backline -p 8080:8080 xlpeabrain/backline
+
+
+#RESULT=$(curl http://localhost:8080/actuator/health)
+#echo $RESULT
+#if [[ $RESULT == *"UP"* ]];
+#then
+#  echo "Up"
+#  exit 0
+#else
+#  echo "Down"
+#  exit 1
+#fi
