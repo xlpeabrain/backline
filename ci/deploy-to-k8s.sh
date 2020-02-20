46b6111c596e3d71010c650bf793bb237e86f4c8
Jason Hoong Kit Lee <hklee@pivotal.io>
2020-02-20 10:35:51 +0800

    test kubectl

--- ci/deploy-to-k8s.sh
+++ ci/deploy-to-k8s.sh
@@ -4,9 +4,9 @@ set -e
 cd backline-src
 
 #Setup Kubeconfig
-kubectl config --kubeconfig=kconfig set-cluster k8s-cluster --server={{k8s-api}} --insecure-skip-tls-verify
-kubectl config --kubeconfig=kconfig set-credentials {{k8s-user}} --client-certificate={{k8s-client-cert}} --client-key={{k8s-client-key}}
-kubectl config --kubeconfig=kconfig set-context ci --cluster=development --namespace={{k8s-namespace}} --user={{k8s-user}}
+kubectl config --kubeconfig=kconfig set-cluster k8s-cluster --server=((k8s-api)) --insecure-skip-tls-verify
+kubectl config --kubeconfig=kconfig set-credentials ((k8s-user)) --client-certificate=((k8s-client-cert)) --client-key=((k8s-client-key))
+kubectl config --kubeconfig=kconfig set-context ci --cluster=development --namespace=((k8s-namespace)) --user=((k8s-user))
 
 #Test access to cluster
 kubectl cluster-info
