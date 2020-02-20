518cd71edf3604d79d5fec6deb5f7ae3a61e621c
Jason Hoong Kit Lee <hklee@pivotal.io>
2020-02-20 10:25:56 +0800

    Deploy phase

---
+++ ci/deploy-to-k8s.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+# Folder is according to the name of the concourse resource calling this script
+cd backline-src
+
+#Setup Kubeconfig
+kubectl config --kubeconfig=kconfig set-cluster k8s-cluster --server={{k8s-api}} --insecure-skip-tls-verify
+kubectl config --kubeconfig=kconfig set-credentials {{k8s-user}} --client-certificate={{k8s-client-cert}} --client-key={{k8s-client-key}}
+kubectl config --kubeconfig=kconfig set-context ci --cluster=development --namespace={{k8s-namespace}} --user={{k8s-user}}
+
+#Test access to cluster
+kubectl cluster-info
+
+#
+
