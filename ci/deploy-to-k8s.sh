4c16ab22c3fb751dabece3b3cc781e1ba56a77b4
Jason Hoong Kit Lee <hklee@pivotal.io>
2020-02-20 13:09:56 +0800

    Update ReadMe

--- ci/deploy-to-k8s.sh
+++ ci/deploy-to-k8s.sh
@@ -3,13 +3,11 @@ set -e
 # Folder is according to the name of the concourse resource calling this script
 cd backline-src
 ls
-env
-echo ""
 
 #Setup Kubeconfig
 kubectl config --kubeconfig=kconfig set-cluster k8s-cluster --server=$K8S_API --insecure-skip-tls-verify
 kubectl config --kubeconfig=kconfig set-credentials $K8S_USER --client-certificate=$K8S_CLIENT_CERT --client-key=$K8S_CLIENT_KEY
-kubectl config --kubeconfig=kconfig set-context ci --cluster=development --namespace=$K8S_NAMESPACE --user=$K8S_USER
+kubectl config --kubeconfig=kconfig set-context default --cluster=development --namespace=$K8S_NAMESPACE --user=$K8S_USER
 
 #Test access to cluster
 kubectl cluster-info
