3162cfe745935ac47fc2bb8e19a9280f689560bc
Jason Hoong Kit Lee <hklee@pivotal.io>
2020-02-21 19:07:14 +0800

    Update ReadMe

--- ci/deploy-to-k8s.sh
+++ ci/deploy-to-k8s.sh
@@ -5,12 +5,17 @@ cd backline-src
 echo "Environment Variables"
 env
 
+echo $K8S_CLIENT_CERT > /tmp/certificate
+echo $K*S_CLIENT_KEY > /tmp/key
+
+cat /tmp/certificate
+
 echo "### Setting up kubeconfig"
 touch /tmp/config.yml
 #Setup Kubeconfig
 kubectl config set-cluster k8s-cluster --server="$K8S_API" --insecure-skip-tls-verify --kubeconfig=/tmp/config.yml
 echo "$K8S_CLIENT_CERT"
-kubectl config set-credentials $K8S_USER --client-certificate="$K8S_CLIENT_CERT" --client-key="$K8S_CLIENT_KEY" --kubeconfig=/tmp/config.yml
+kubectl config set-credentials $K8S_USER --client-certificate=/tmp/certificate --client-key=/tmp/key --kubeconfig=/tmp/config.yml
 kubectl config set-context default --cluster=k8s-cluster --namespace="$K8S_NAMESPACE" --user="$K8S_USER" --kubeconfig=/tmp/config.yml
 echo "### Kube config file"
 cat /tmp/config.yml
