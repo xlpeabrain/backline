6f79ab67739045a363bf9a95a8ba3f8b1eeaeac5
Jason Hoong Kit Lee <hklee@pivotal.io>
2020-02-21 10:56:40 +0800

    Update ReadMe

--- ci/deploy-to-k8s.sh
+++ ci/deploy-to-k8s.sh
@@ -2,12 +2,17 @@
 set -e
 # Folder is according to the name of the concourse resource calling this script
 cd backline-src
+echo "Environment Variables"
+env
 
+echo "### Setting up kubeconfig"
 #Setup Kubeconfig
 kubectl config --kubeconfig=kconfig set-cluster k8s-cluster --server=$K8S_API --insecure-skip-tls-verify
 kubectl config --kubeconfig=kconfig set-credentials $K8S_USER --client-certificate=$K8S_CLIENT_CERT --client-key=$K8S_CLIENT_KEY
 kubectl config --kubeconfig=kconfig set-context default --cluster=development --namespace=$K8S_NAMESPACE --user=$K8S_USER
 
+echo "### Available contexts"
+kubectl config get-contexts
 kubectl config use-context default
 kubectl config current-context
 ls -la /
