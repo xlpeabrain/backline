a35579cf6aa8acbbfa5db3e28d5b3de983a7ec00
Jason Hoong Kit Lee <hklee@pivotal.io>
2020-02-21 18:23:03 +0800

    Update ReadMe

--- ci/deploy-to-k8s.sh
+++ ci/deploy-to-k8s.sh
@@ -8,9 +8,9 @@ env
 echo "### Setting up kubeconfig"
 touch /tmp/config
 #Setup Kubeconfig
-kubectl config --kubeconfig=kconfig set-cluster k8s-cluster --server=$K8S_API --insecure-skip-tls-verify --kubeconfig=/tmp/config
-kubectl config --kubeconfig=kconfig set-credentials $K8S_USER --client-certificate=$K8S_CLIENT_CERT --client-key=$K8S_CLIENT_KEY --kubeconfig=/tmp/config
-kubectl config --kubeconfig=kconfig set-context default --cluster=k8s-cluster --namespace=$K8S_NAMESPACE --user=$K8S_USER --kubeconfig=/tmp/config
+kubectl config set-cluster k8s-cluster --server=$K8S_API --insecure-skip-tls-verify --kubeconfig=/tmp/config
+kubectl config set-credentials $K8S_USER --client-certificate=$K8S_CLIENT_CERT --client-key=$K8S_CLIENT_KEY --kubeconfig=/tmp/config
+kubectl config set-context default --cluster=k8s-cluster --namespace=$K8S_NAMESPACE --user=$K8S_USER --kubeconfig=/tmp/config
 
 echo "### Available contexts"
 kubectl config get-contexts --kubeconfig=/tmp/config
@@ -20,7 +20,7 @@ kubectl config current-context --kubeconfig=/tmp/config
 cat /tmp/config
 
 #Test access to cluster
-kubectl cluster-info --kubeconfig=/tmp/config
+kubectl cluster-info --kubeconfig='/tmp/config'
 
 #
 
