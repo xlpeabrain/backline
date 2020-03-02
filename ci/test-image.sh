bb053e115404601c54a2ead5ea44fb11c0a59397
Jason Hoong Kit Lee <hklee@pivotal.io>
2020-03-02 15:38:54 +0800

    add todo

--- ci/test-image.sh
+++ ci/test-image.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-
-
 source /docker-lib.sh
 start_docker
 
@@ -9,16 +7,15 @@ cd backline-src
 ls
 docker run -d --rm --name backline -p 8080:8080 xlpeabrain/backline
 sleep 15
-#curl http://localhost:8080/actuator/health
-
 
 RESULT=$(curl http://localhost:8080/actuator/health)
 echo $RESULT
-#if [[ $RESULT == *_"UP"_* ]];
-#then
-#  echo "Up"
-#  exit 0
-#else
-#  echo "Down"
-#  exit 1
-#fi
+
+if [[ $RESULT == *_UP_* ]];
+then
+  echo "Up"
+  exit 0
+else
+  echo "Down"
+  exit 1
+fi
