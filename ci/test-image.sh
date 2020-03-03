56c3c77c6a65d79ce4e167d5b314ed761d617e75
Jason Hoong Kit Lee <hklee@pivotal.io>
2020-03-03 12:13:55 +0800

    test error

--- ci/test-image.sh
+++ ci/test-image.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-#source /docker-lib.sh
-#start_docker
-#pwd
-#
-#cd backline-src
-##ls
+source /docker-lib.sh
+start_docker
+
 docker run -d --rm --name backline -p 8080:8080 xlpeabrain/backline
 sleep 15
 
@@ -15,7 +12,6 @@ echo $RESULT
 if [[ $RESULT == *"UP"* ]];
 then
   echo "Up"
-  cd ../
   mkdir gen
   echo $RESULT >> status.txt
   exit 0
