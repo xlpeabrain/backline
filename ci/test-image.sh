d701d80c4b1f8399f86ea8a5b8fee7a226708f97
Jason Hoong Kit Lee <hklee@pivotal.io>
2020-03-03 11:46:38 +0800

    Update ReadMe

--- ci/test-image.sh
+++ ci/test-image.sh
@@ -2,18 +2,22 @@
 
 source /docker-lib.sh
 start_docker
+pwd
 
 cd backline-src
-ls
+#ls
 docker run -d --rm --name backline -p 8080:8080 xlpeabrain/backline
 sleep 15
 
 RESULT=$(curl http://localhost:8080/actuator/health)
 echo $RESULT
 
-if [[ $RESULT == *_UP_* ]];
+if [[ $RESULT = *UP* ]];
 then
   echo "Up"
+  cd ../
+  mkdir gen
+  echo $RESULT >> status.txt
   exit 0
 else
   echo "Down"
