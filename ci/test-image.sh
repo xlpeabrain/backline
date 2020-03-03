e1192f988aba1d0110ff259f797b1b3a82d2cc86
Jason Hoong Kit Lee <hklee@pivotal.io>
2020-03-03 15:54:12 +0800

    Update ReadMe

--- ci/test-image.sh
+++ ci/test-image.sh
@@ -7,17 +7,16 @@ docker run -d --rm --name backline -p 8080:8080 xlpeabrain/backline
 sleep 15
 
 RESULT=$(curl http://localhost:8080/actuator/health)
-echo $RESULT
+echo "$RESULT"
 
 # Define the sanity check logic for container validation here
-if test $RESULT == "UP";
+if test "$RESULT" == '{"status":"UP"}';
 then
   echo "Up"
   cd gen
-#  echo $RESULT >> status.txt
+  echo "$RESULT" >> status.txt
   exit 0
 else
-  echo "FAILED"
   echo "Down"
   exit 1
 fi
